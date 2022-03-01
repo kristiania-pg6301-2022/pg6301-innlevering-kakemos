47cbddf0a60000a7f00fa15682e56c5d8dcefcf5
kakemos <cjkbsn@gmail.com>
2022-03-01 11:50:59 +0100

    Reformat questions.js

--- client/quizApp.jsx
+++ client/quizApp.jsx
@@ -1,1 +1,21 @@
-export function QuizApp
+import React, {useState} from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+function FrontPage() {
+    return null;
+}
+
+function ShowQuestion() {
+    return null;
+}
+
+export function QuizApp() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<FrontPage/>}/>
+                <Route path="/question" element={<ShowQuestion/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
