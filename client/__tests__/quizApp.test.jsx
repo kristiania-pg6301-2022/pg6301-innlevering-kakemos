b5a85dbce0b17a49ea100204d408aa7007131e94
kakemos <cjkbsn@gmail.com>
2022-03-03 19:32:38 +0100

    Final test fail

--- client/__tests__/quizApp.test.jsx
+++ client/__tests__/quizApp.test.jsx
@@ -2,43 +2,43 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { MemoryRouter } from "react-router-dom";
 import { FrontPage } from "../quizApp.jsx";
-import { Simulate } from "react-dom/test-utils";
+
+// testene vil ikke kjøre, da vi får error vi ikke klarer å løse.
+// Det ble derfor vanskelig å skrive flere tester
 
 const question = {
-  question: "Are you happy?",
+  question: "What is 2 + 2?",
   answers: {
-    answer_a: "Yes",
-    answer_b: "No",
-    answer_c: "Maybe",
+    answer_a: "22",
+    answer_b: "4",
+    answer_c: "3",
   },
   correct_answers: {
-    answer_a_correct: "true",
-    answer_b_correct: "false",
+    answer_a_correct: "false",
+    answer_b_correct: "true",
     answer_c_correct: "false",
   },
 };
 
 describe("quiz application", () => {
-  it("shows front page"),
-    () => {
-      const element = document.createElement("div");
-      ReactDOM.render(<FrontPage />, element);
+  it("shows front page", () => {
+    const element = document.createElement("div");
+    ReactDOM.render(<FrontPage />, element);
 
-      expect(element.querySelector("h1").innerHTML).toEqual("TIME FOR QUIZ!");
-    };
+    expect(element.querySelector("h1").innerHTML).toEqual("TIME FOR QUIZ!");
+    expect(element.innerHTML).toMatchSnapshot();
+  });
+
+  it("shows question", () => {
+    const element = document.createElement("div");
+    ReactDOM.render(
+      <MemoryRouter initialEntries={["/question"]}>
+        <QuestionContext.Provider value={{ randomQuestion: () => question }}>
+          <Application />
+        </QuestionContext.Provider>
+      </MemoryRouter>,
+      element
+    );
+    expect(element.innerHTML).toMatchSnapshot();
+  });
 });
-
-// describe("front page", () => {
-//   it("shows question", () => {
-//     const element = document.createElement("div");
-//     ReactDOM.render(
-//       <MemoryRouter initialEntries={["/question"]}>
-//         <QuestionContext.Provider value={{ randomQuestion: () => question }}>
-//           <Application />
-//         </QuestionContext.Provider>
-//       </MemoryRouter>,
-//       element
-//     );
-//     expect(element.innerHTML).toMatchSnapshot();
-//   });
-// });
