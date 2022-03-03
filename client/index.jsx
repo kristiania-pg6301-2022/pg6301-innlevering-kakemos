d7e182165ffd2a1b6b91fee408fbbabd231e505d
kakemos <cjkbsn@gmail.com>
2022-03-03 10:10:02 +0100

    Running application

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { QuizApp } from "./quizApp";
+import {BrowserRouter} from "react-router-dom";
 
-ReactDOM.render(<QuizApp />, document.getElementById("app"));
+ReactDOM.render(<BrowserRouter><QuizApp /></BrowserRouter>, document.getElementById("app"));
--- client/quizApp.jsx
+++ client/quizApp.jsx
@@ -1,5 +1,9 @@
 import React, {useState} from "react";
-import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes, useNavigate} from "react-router-dom";
+import {isCorrectAnswer, randomQuestion} from "../server/questions.js";
+import {useContext} from "../dist/index.d240bd5c";
+
+export const QuestionContext = React.createContext({randomQuestion})
 
 function FrontPage() {
     return <div>
@@ -11,16 +15,66 @@ function FrontPage() {
 }
 
 function ShowQuestion() {
-    return null;
+    const navigate = useNavigate();
+    const [question] = useState(randomQuestion());
+
+    function handleAnswer(answer) {
+        if (isCorrectAnswer(question, answer)) {
+            navigate("/answer/correct");
+            return <ShowAnswer question={question} answer={answer}/>;
+        } else {
+            navigate("/answer/wrong");
+            return <ShowAnswer question={question} answer={answer}/>;
+        }
+    }
+
+    return <>
+        <h2>Can you answer this question? ...</h2>
+        <h1>{question.question}</h1>
+        <div>{Object.keys(question.answers)
+            .filter(a => question.answers[a])
+            .map(a => <p key={a}>
+                <input type={"radio"}
+                       name={"answerChoice"}
+                       value={a}
+                />
+                <label>{question.answers[a]}</label>
+            </p>)}
+            <button
+                onClick={() => handleAnswer(document.querySelector('input[name="answerChoice"]:checked').value)}>Answer
+            </button>
+            <p></p>
+            <div><Link to={"/"}>
+                <button>Back to front page</button>
+            </Link></div>
+        </div>
+    </>;
+}
+
+function ShowAnswer() {
+    return <div>
+        <Routes>
+            <Route path={"/correct"} element={<h1>YAY, you answered correctly!</h1>}/>
+            <Route path={"/wrong"} element={<h1>Whoops! The answer was wrong!</h1>}/>
+        </Routes>
+        <div><Link to={"/question"}>
+            <button>Answer another question!</button>
+        </Link></div>
+        <p></p>
+        <div><Link to={"/"}>
+            <button>Back to front page</button>
+        </Link></div>
+    </div>
 }
 
 export function QuizApp() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<FrontPage/>}/>
-                <Route path="/question" element={<ShowQuestion/>}/>
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path={"/"}
+                   element={<FrontPage />}/>
+            <Route path={"/question"} element={<ShowQuestion />}/>
+            <Route path={"/answer/*"} element={<ShowAnswer/>}/>
+            <Route path={"*"} element={<h1>Not found</h1>}/>
+        </Routes>
     );
 }
