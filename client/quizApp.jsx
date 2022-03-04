08870961057c98a465c898d20ee535b522a72ca5
kakemos <cjkbsn@gmail.com>
2022-03-04 18:47:44 +0100

    working app

--- client/quizApp.jsx
+++ client/quizApp.jsx
@@ -1,15 +1,8 @@
-import React, { useState } from "react";
-import {
-  BrowserRouter,
-  Link,
-  Route,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
-import { isCorrectAnswer, randomQuestion } from "../server/questions.js";
-// import { useContext } from "../dist/index.d240bd5c";
+import React, { useState, useEffect } from "react";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
+import { randomQuestion } from "../server/questions.js";
 
-// export const QuestionContext = React.createContext({ randomQuestion });
+export const QuestionContext = React.createContext({ randomQuestion });
 
 export function FrontPage() {
   return (
@@ -23,33 +16,94 @@ export function FrontPage() {
   );
 }
 
-function ShowQuestion(question) {
-  //const navigate = useNavigate();
-  //const { randomQuestion } = useContext(QuestionContext);
-  //const [question] = useState(randomQuestion());
+function useLoading(loadingFunction) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [data, setData] = useState();
+
+  async function load() {
+    setLoading(true);
+    try {
+      setData(await loadingFunction());
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  return { loading, error, data };
+}
+
+export class HttpError extends Error {
+  constructor(status, statusText) {
+    super(statusText);
+    this.status = status;
+  }
+}
+
+export async function fetchJSON(url) {
+  const res = await fetch(url);
+  if (res.status === 204) {
+    return null;
+  } else if (res.ok) {
+    return await res.json();
+  } else {
+    throw new HttpError(res.status, res.statusText);
+  }
+}
+
+// async function fetchJSON(url) {
+//   const res = await fetch(url);
+//   if (!res.ok) {
+//     throw new Error(`Failed to load ${res.status}: ${res.statusText}`);
+//   }
+//   return await res.json();
+// }
+
+function ShowQuestion() {
+  const { loading, error, data } = useLoading(async () =>
+    fetchJSON("/api/question")
+  );
+  const navigate = useNavigate();
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error loading question: {error.toString()}</div>;
+
+  const { id, question, answers } = data;
 
   async function handleAnswer(answer) {
-    const {id} = question;
-    const res = await fetch("/api/question", {
-        method: "post",
-        headers: {
-            "content-type": "application/json",
-        },
-        body: JSON.stringify({id, answer}),
+    const response = await fetch("/api/question", {
+      method: "post",
+      body: JSON.stringify({ id, answer }),
+      headers: { "content-type": "application/json" },
     });
+    const result = await response.json();
+
+    if (result.result === "true") {
+      navigate("/answer/correct");
+      return <ShowAnswer question={question} answer={answer} />;
+    } else {
+      navigate("/answer/wrong");
+      return <ShowAnswer question={question} answer={answer} />;
+    }
   }
 
   return (
     <>
       <h2>Can you answer this question? ...</h2>
-      <h1>{question.question}</h1>
+      <h1>{question}</h1>
       <div>
-        {Object.keys(question.answers)
-          .filter((a) => question.answers[a])
+        {Object.keys(answers)
+          .filter((a) => answers[a])
           .map((a) => (
             <p key={a}>
               <input type={"radio"} name={"answerChoice"} value={a} />
-              <label>{question.answers[a]}</label>
+              <label>{answers[a]}</label>
             </p>
           ))}
         <button
@@ -70,26 +124,6 @@ function ShowQuestion(question) {
       </div>
     </>
   );
-}
-
-function QuestionComponent(){
-    const [question, setQuestion] = useState();
-
-    async function handleLoadQuestion(){
-        const res = await fetch("api/question");
-        setQuestion(await res.json());
-    }
-
-    if(!question){
-        return (
-            <div>
-                <button onClick={handleLoadQuestion}>New Question</button>
-            </div>
-        );
-    }
-
-    return <ShowQuestion question={question} />;
-
 }
 
 function ShowAnswer() {
@@ -124,7 +158,7 @@ export function QuizApp() {
   return (
     <Routes>
       <Route path={"/"} element={<FrontPage />} />
-      <Route path={"/question"} element={<QuestionComponent />} />
+      <Route path={"/question"} element={<ShowQuestion />} />
       <Route path={"/answer/*"} element={<ShowAnswer />} />
       <Route path={"*"} element={<h1>Not found</h1>} />
     </Routes>
